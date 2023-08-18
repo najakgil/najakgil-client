a32d0786cd4d469899c1a67e83faec334da9b4ba
anonymousrecords <a.Records2022@gmail.com>
2023-08-18 21:23:37 +0900

    feat : charactertoolitem

---
+++ src/make/CharacterToolItemList.ts
@@ -0,0 +1,38 @@
+import { CharacterItemList } from "./CharacterItemList"
+export const CharacterToolItemList = [
+    {
+        name: '겨울 외투',
+        listname: 'winter',
+        decorateitemlist: CharacterItemList.winter
+    },
+    {
+        name: '바캉스룩',
+        listname: 'summer',
+        decorateitemlist: CharacterItemList.summer
+    },
+    {
+        name: '의사 가운',
+        listname: 'scrub',
+        decorateitemlist: CharacterItemList.scrub
+    },
+    {
+        name: '정장',
+        listname: 'suit',
+        decorateitemlist: CharacterItemList.suit
+    },
+    {
+        name: '학위복',
+        listname: 'graduationsuit',
+        decorateitemlist: CharacterItemList.graduationsuit
+    },
+    {
+        name: '여자 한복',
+        listname: 'fhanbok',
+        decorateitemlist: CharacterItemList.fhanbok
+    },
+    {
+        name: '남자 한복',
+        listname: 'mhanbok',
+        decorateitemlist: CharacterItemList.mhanbok
+    },
+]
