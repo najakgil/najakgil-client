4e05779218249dd71862dbd4a74a3d342f6b422a
anonymousrecords <a.Records2022@gmail.com>
2023-08-18 21:23:15 +0900

    feat : 캐릭터 아이템 리스트

---
+++ src/make/CharacterItemList.ts
@@ -0,0 +1,44 @@
+export const CharacterItemList = {
+    "winter" : [
+        "/assets/image/winter_1.svg",
+        "/assets/image/winter_2.svg",
+        "/assets/image/winter_3.svg",
+        "/assets/image/winter_4.svg",
+    ],
+    "summer" : [
+        "/assets/image/summer_1.svg",
+        "/assets/image/summer_2.svg",
+        "/assets/image/summer_3.svg",
+        "/assets/image/summer_4.svg",
+    ],
+    "scrub" : [
+        "/assets/image/scrub_1.svg",
+        "/assets/image/scrub_2.svg",
+        "/assets/image/scrub_3.svg",
+        "/assets/image/scrub_4.svg",
+    ],
+    "suit" : [
+        "/assets/image/suit_1.svg",
+        "/assets/image/suit_2.svg",
+        "/assets/image/suit_3.svg",
+        "/assets/image/suit_4.svg",
+    ],
+    "graduationsuit" : [
+        "/assets/image/graduationsuit_1.svg",
+        "/assets/image/graduationsuit_2.svg",
+        "/assets/image/graduationsuit_3.svg",
+        "/assets/image/graduationsuit_4.svg",
+    ],
+    "fhanbok" : [
+        "/assets/image/fhanbok_1.svg",
+        "/assets/image/fhanbok_2.svg",
+        "/assets/image/fhanbok_3.svg",
+        "/assets/image/fhanbok_4.svg",
+    ],
+    "mhanbok" : [
+        "/assets/image/mhanbok_1.svg",
+        "/assets/image/mhanbok_2.svg",
+        "/assets/image/mhanbok_3.svg",
+        "/assets/image/mhanbok_4.svg",
+    ],
+}
