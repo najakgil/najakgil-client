f3cc6150f588da82664aaa5cecf26ca3d49cfc1f
anonymousrecords <a.Records2022@gmail.com>
2023-08-18 21:18:28 +0900

    feat : Header 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header: React.FC = () => {
+    return (
+        <StyledHeader>
+            <Logo>나만의 총장님</Logo>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.div`
+    width: 360px;
+    height: 45px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 16px;
+    border-bottom: 1px solid #F0F0F0;
+`
+
+const Logo = styled.div`
+    color: #2294FF;
+    text-align: center;
+    font-family: Noto Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+`
+
+export default Header;
