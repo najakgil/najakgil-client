4622d6ee9bade98c3e6c134e985f59fa073b6dd0
anonymousrecords <a.Records2022@gmail.com>
2023-08-18 21:24:55 +0900

    feat : 프로젝트 기본 설정

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route  } from "react-router-dom"
+import Make from "./make/Make.container";
+import Header from "./components/Header";
+import Preview from "./preview/Preview.container";
+import Home from "./home/Home.container";
+import Mypage from "./mypage/Mypage.container";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/" element={<Make/>}/>
+          <Route path="/preview" element={<Preview/>}/>
+          <Route path="/mypage" element={<Mypage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
