c8b1987bc8d03e2706870d6fd708b9d6b30b8d8d
anonymousrecords <a.Records2022@gmail.com>
2023-08-28 21:18:09 +0900

    style : Header 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,31 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const Header: React.FC = () => {
-    return (
-        <StyledHeader>
-            <img src="/assets/icon/logo.svg" style={{width:"30px"}}/>
-            {/* <Logo>나만의 총장님</Logo> */}
-        </StyledHeader>
-    );
+  return (
+    <StyledHeader>
+      <>
+        <img src="/assets/icon/logo.svg" style={{ width: "30px" }} />
+      </>
+    </StyledHeader>
+  );
 };
 
 const StyledHeader = styled.div`
-    width: 360px;
-    height: 45px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    padding-left: 16px;
-    border-bottom: 1px solid #F0F0F0;
-`
-
-// const Logo = styled.div`
-//     color: #2294FF;
-//     text-align: center;
-//     font-family: Noto Sans;
-//     font-size: 16px;
-//     font-style: normal;
-//     font-weight: 600;
-// `
+  width: 360px;
+  height: 45px;
+  /* display: flex;
+  justify-content: flex-start;
+  align-items: center; */
+  padding-left: 16px;
+  border-bottom: 1px solid #f0f0f0;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  /* margin: auto 0; */
+  background: white;
+`;
 
 export default Header;
