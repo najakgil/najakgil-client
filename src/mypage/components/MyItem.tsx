5307df8daa1726f02327e29a1302c0f73a58db75
anonymousrecords <a.Records2022@gmail.com>
2023-09-01 23:24:29 +0900

    feat : MyPage > 유저가 만든 캐릭터 모아보기

---
+++ src/mypage/components/MyItem.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import ModalOverlay from "./ModalOverlay";
+import DeleteModal from "./DeleteModal";
+import { useState } from "react";
+
+interface CardItemProps {
+  src: string;
+}
+
+export default function MyItem(props: CardItemProps) {
+  // Delete 모달창 관리
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const showDeleteModalHandler = () => {
+    setShowDeleteModal(true);
+  };
+
+  const hideDeleteModalHandler = () => {
+    setShowDeleteModal(false);
+  };
+
+  return (
+    <>
+      <CardItemBox>
+        <DeleteButton onClick={showDeleteModalHandler}>
+          <img src="/assets/icon/delete_button.svg" alt="Delete" />
+        </DeleteButton>
+        <Card src={props.src} alt="Card" />
+      </CardItemBox>
+      {/* Delete 모달창 */}
+      <ModalOverlay blur onHideModal={hideDeleteModalHandler} show={showDeleteModal}>
+        <DeleteModal onHideModal={hideDeleteModalHandler} />
+      </ModalOverlay>
+    </>
+  );
+}
+
+const CardItemBox = styled.div`
+  width: 165px;
+  height: 165px;
+  position: relative;
+`;
+
+const Card = styled.img`
+  position: relative;
+  z-index: 10;
+  width: 165px;
+  height: 165px;
+  border-radius: 3px;
+  border: 0.5px solid #d9d9d9;
+`;
+
+const DeleteButton = styled.div`
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  width: 24px;
+  height: 24px;
+  z-index: 20;
+`;
---
+++ src/mypage/components/MyItemBox.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { CardItemList } from "../../home/components/CardItemList";
+import MyItem from "./MyItem";
+
+export default function MyItemBox() {
+  return (
+    <StyledMyItem>
+      <MyItemContainer>
+        {CardItemList.map((item, index) => (
+          <MyItem key={index} src={item.src} />
+        ))}
+      </MyItemContainer>
+    </StyledMyItem>
+  );
+}
+
+const StyledMyItem = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MyItemContainer = styled.div`
+  width: 100%;
+  height: 689px;
+  background: green;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 10px;
+  gap: 10px;
+  overflow-y: scroll;
+`;
