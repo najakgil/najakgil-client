b5b2208f041fcf665e147bd5fa05401fa7b121d7
anonymousrecords <a.Records2022@gmail.com>
2023-09-01 23:27:41 +0900

    feat : Make > Decorate > 스티커 구현

---
+++ src/make/StickerItemList.ts
@@ -0,0 +1,8 @@
+export const StickerItemList = [
+    { src : "/assets/image/sticker_1.png" },
+    { src : "/assets/image/sticker_2.png" },
+    { src : "/assets/image/sticker_3.png" },
+    { src : "/assets/image/sticker_4.png" },
+    { src : "/assets/image/sticker_5.png" },
+    { src : "/assets/image/sticker_6.png" },
+];
--- src/recoil/CharacterChoiceAtom.tsx
+++ src/recoil/CharacterChoiceAtom.tsx
@@ -2,5 +2,5 @@ import { atom } from "recoil";
 
 export const CharacterChoiceAtom = atom({
   key: "characterState",
-  default: "/assets/image/default.svg",
+  default: "/assets/image/default.png",
 });
