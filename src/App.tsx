e11a948456ccf116140655968d485e4709fbba2e
anonymousrecords <a.Records2022@gmail.com>
2023-09-09 23:40:59 +0900

    feat : Route 재등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import { BrowserRouter, Routes, Route  } from "react-router-dom"
-// import Make from "./make/Make.container";
-// import Header from "./components/Header";
-// import Preview from "./preview/Preview.container";
-// import Home from "./home/Home.container";
-// import Mypage from "./mypage/Mypage.container";
-import ShowPage from "./show/ShowPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Make from "./make/Make.container";
+import Preview from "./preview/Preview.container";
+import Home from "./home/Home.container";
+import Mypage from "./mypage/Mypage.container";
+import ShowPage from "./show/ShowPage";
+import Redirect from "./redirect/Redirect";
 
 const App: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/home" element={<Home/>}/>
-          <Route path="/" element={<Make/>}/>
-          <Route path="/preview" element={<Preview/>}/>
-          <Route path="/mypage" element={<Mypage/>}/> */}
-          <Route path="/" element={<Make/>}/>
-          <Route path="/show" element={<ShowPage/>}/>
+          <Route path="/" element={<Make />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/preview" element={<Preview />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/show" element={<ShowPage />} />
+          <Route path="/redirect" element={<Redirect />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
 export default App;
