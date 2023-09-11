6f52d16b452750fdcff2356e66faa4f2270c92f9
anonymousrecords <a.Records2022@gmail.com>
2023-09-11 16:50:24 +0900

    feat : 타입 설정

--- src/data/type.ts
+++ src/data/type.ts
@@ -1,6 +1,24 @@
-export type PhotoChart = {
-  page: number,
-  size: number,
-  standard: string,
-  imgUrl: string,
+export interface APIResponse<T> {
+  code: number;
+  isSuccess: boolean;
+  message: string;
+  result: T;
+}
+export interface Page<T> {
+  content: T;
+  last: boolean;
+  number: number;
+}
+
+export type Photo = {
+  photo_id: number;
+  imgUrl: string;
 };
+
+export type PhotoChart = {
+  photo_id: number;
+  imgUrl: string;
+  likes: number;
+};
+
+export type Standard = "likes" | "createdAt";
