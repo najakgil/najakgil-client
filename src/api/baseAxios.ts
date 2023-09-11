bd6f5b9934b1101c583ad59574a3f13e19e85f9a
anonymousrecords <a.Records2022@gmail.com>
2023-09-11 16:49:15 +0900

    fix : 불필요한 코드 제거

--- src/api/baseAxios.ts
+++ src/api/baseAxios.ts
@@ -2,8 +2,4 @@ import axios from "axios";
 
 export const baseAxios = axios.create({
   baseURL: "http://15.164.68.208:8080/",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": true,
-  },
 });
