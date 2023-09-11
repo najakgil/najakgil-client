40a3dd4b6e5f5a75115d86100a5c837b4dc00150
anonymousrecords <a.Records2022@gmail.com>
2023-09-11 16:50:42 +0900

    chore : HOC 시도

---
+++ src/hoc/withAuth.ts
@@ -0,0 +1,15 @@
+// import { ComponentType } from "react";
+
+// export const withAuth: React.FC =
+//   (Component: ComponentType) =>
+//   <P extends {}>(props: P) => {
+//     /* 권한 분기 */
+//     useEffect(() => {
+//       if (!localStorage.getItem("accessToken")) {
+//         alert("로그인 후 이용이 가능합니다.");
+//         router.push("/23-04-login-check");
+//       }
+//     }, []);
+
+//     return <Component {...props} />;
+//   };
