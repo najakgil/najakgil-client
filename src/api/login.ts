537704a0a2f6f6c3e39441804ed5fe094398a328
anonymousrecords <a.Records2022@gmail.com>
2023-09-20 19:32:05 +0900

    mod : 카카오로그인 API 변경

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,6 +1,12 @@
+// import { baseAxios } from "./baseAxios";
+
+// export const getLogin = (code: string) => {
+//   // return baseAxios.get(`/api/v1/oauth/kakao?code=${code}`);
+//   return baseAxios.get(`/oauth2/authorization/kakao?code=${code}`);
+// };
+
 import { baseAxios } from "./baseAxios";
 
 export const getLogin = (code: string) => {
-  // return baseAxios.get(`/api/v1/oauth/kakao?code=${code}`);
-  return baseAxios.get(`/oauth2/authorization/kakao?code=${code}`);
+  return baseAxios.get(`api/oauth/kakao?code=${code}`);
 };
