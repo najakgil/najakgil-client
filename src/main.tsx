8c450cafafd76a26cff0d322e8f47db0d364e5dd
anonymousrecords <a.Records2022@gmail.com>
2023-10-02 23:31:01 +0900

    feat : recoil-persist 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from "recoil";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { CookiesProvider } from "react-cookie";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </CookiesProvider>
   </React.StrictMode>
 );
--- src/recoil/PreviewCardAtom.tsx
+++ src/recoil/PreviewCardAtom.tsx
@@ -1,6 +1,13 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "localStorage",
+  storage: localStorage,
+})
 
 export const PreviewCardAtom = atom({
   key: "previewCardState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
