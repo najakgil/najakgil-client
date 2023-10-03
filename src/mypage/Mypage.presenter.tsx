e150b91fc9abf6832335616f0f688e0e03ebb2e7
anonymousrecords <a.Records2022@gmail.com>
2023-10-03 22:35:15 +0900

    mod : 로그인 여부에 따른 화면 설정

--- src/mypage/Mypage.presenter.tsx
+++ src/mypage/Mypage.presenter.tsx
@@ -2,15 +2,25 @@ import styled from "styled-components";
 // import MyItemBox from "./components/MyItemBox";
 import Header from "../components/Header";
 import BottomNav from "../components/BottomNav";
+import { getCookie } from "../util/cookie";
 import NotSignIn from "./components/NotSignIn";
+import NoItem from "./components/NoItem";
+
+// import NotSignIn from "./components/NotSignIn";
+// import MyItemBox from "./components/MyItemBox";
+// import NoItem from "./components/NoItem";
 
 const MyPageUI: React.FC = () => {
+  const cookie = getCookie("jwtToken");
+  
   return (
     <>
       <StyledMypage>
         <Header />
+        {cookie ? <NoItem /> : <NotSignIn />}
+        {/* <NotSignIn /> */}
+        {/* <NoItem /> */}
         {/* <MyItemBox /> */}
-        <NotSignIn />
         <BottomNav type={"mypage"} />
       </StyledMypage>
     </>
