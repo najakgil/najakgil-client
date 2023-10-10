7e9d90e5c69517048fa5689b68eb05bebb8f8032
anonymousrecords <a.Records2022@gmail.com>
2023-10-10 09:32:21 +0900

    ✨ [FEAT] : 좋아요 기능 구현

--- src/home/components/CardItem.tsx
+++ src/home/components/CardItem.tsx
@@ -1,41 +1,48 @@
 import styled from "styled-components";
 import { useState } from "react";
+import { baseAxios } from "../../api/baseAxios";
+import { getCookie } from "../../util/cookie";
 
 interface CardItemProps {
   src: string;
+  photoId: number;
+  likeCount: number;
 }
 
 export default function CardItem(props: CardItemProps) {
-  const [like, setLike] = useState([0]);
+  const [like, setLike] = useState<number[]>([0]);
+  const [liked, setLiked] = useState<boolean>(false);
 
-  const handleLikeClick = (index: number) => {
-    const likeCnt = [...like];
-    likeCnt[index]++;
-    setLike(likeCnt);
+  const handleLikeClick = async (index: number) => {
+    const userId = getCookie("userId");
+    const photoId = props.photoId;
+
+    // 이미 좋아요를 누른 상태면 더 이상 처리하지 않음
+    if (liked) {
+      return;
+    }
+
+    try {
+      const response = await baseAxios.post(`/photo/like/${photoId}/${userId}`);
+      if (response.status === 200) {
+        const updatedLike = [...like];
+        updatedLike[index] = response.data.result.likes_cnt;
+        setLike(updatedLike);
+      }
+      console.log("좋아요 클릭 : ", response);
+    } catch (error) {
+      console.error("좋아요 클릭 > 오류 발생 : ", error);
+    }
   };
-
-  // 서버 연동 시
-  // useEffect(async () => {
-  //   const fetchData = async () => {
-  //     const res = await axios.get(...)
-  //     if (res.data.type === 'liked') setLike(true)
-  //   }
-  //   fetchData()
-  // }, []);
-  
-  // const toggleLike = async (e) => {
-  //   const res = await axios.post(...) // [POST] 사용자가 좋아요를 누름 -> DB 갱신
-  //   setLike(!like)
-  // }
 
   return (
     <CardItemBox>
-      {like.map((count, index) => (
+      {like.map((index) => (
         <LikeBox key={index}>
           <button onClick={() => handleLikeClick(index)}>
             <img src="public/assets/icon/heart.svg" alt="Heart Icon" />
           </button>
-          <div>{count}</div>
+          <div>{props.likeCount}</div>
         </LikeBox>
       ))}
       <Card src={props.src} />
@@ -47,21 +54,20 @@ const CardItemBox = styled.div`
   width: 165px;
   height: 165px;
   position: relative;
-  /* background: red; */
-  /* background-image: url("/assets/image/푸바오.jpeg"); */
 `;
 
 const Card = styled.img`
   position: relative;
-  z-index: 10; 
+  z-index: 10;
   width: 165px;
   height: 165px;
   border-radius: 3px;
   border: 0.5px solid #d9d9d9;
+  object-fit: cover;
 `;
 
 const LikeBox = styled.div`
-  position: absolute; 
+  position: absolute;
   top: 129px;
   left: 104px;
   width: 49px;
@@ -72,7 +78,7 @@ const LikeBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 20; 
+  z-index: 20;
 
   button {
     display: flex;
