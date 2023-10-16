14a5062dad27c31a1476c400c977b8f00e7bcd06
anonymousrecords <a.Records2022@gmail.com>
2023-10-16 21:52:44 +0900

    ✅ [MOD] : BottomNav 코드 위치 변경

--- src/home/Home.container.tsx
+++ src/home/Home.container.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import BottomNav from "../components/BottomNav";
 import HomeUI from "./Home.presenter";
 
 const Home: React.FC = () => {
   return (
     <>
       <HomeUI />
-      <BottomNav type={"home"} />
     </>
   );
 };
