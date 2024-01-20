c431d5fc3fe32441cf6ed7270d8415e5c73da195
anonymousrecords <a.Records2022@gmail.com>
2024-01-21 00:54:25 +0900

    feat : Layout 컴포넌트 구현

---
+++ src/common/layout/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 390px;
+  max-width: 450px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: white;
+  overflow: hidden;
+`;
