bd8fcacb2227cf71c00e42e1d8c3a20f6a9be06d
anonymousrecords <a.Records2022@gmail.com>
2024-01-21 00:54:37 +0900

    feat : Layout 컴포넌트 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { RecoilRoot } from 'recoil';
+import Layout from './common/layout/layout.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <Layout>
+        <App />
+      </Layout>
     </RecoilRoot>
   </React.StrictMode>,
 );
