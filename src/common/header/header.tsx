c661b9efad6c6b2e7300b10643504ba737134abc
anonymousrecords <a.Records2022@gmail.com>
2024-01-23 21:39:58 +0900

    feat : Header 구현

---
+++ src/common/header/header.tsx
@@ -0,0 +1,13 @@
+import * as S from './style';
+import { useNavigate } from 'react-router-dom';
+
+export default function Header() {
+  const navigate = useNavigate();
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.Logo src="/assets/svg/logo.svg" alt="Logo" onClick={() => navigate('/home')}/>
+      </S.Container>
+    </S.Wrapper>
+  );
+}
