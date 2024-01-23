4f8007ef29ba5ba39b577722c15f0ad3c40c9280
anonymousrecords <a.Records2022@gmail.com>
2024-01-23 21:39:43 +0900

    feat : router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-function App() {
-  return '나작길';
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Make from './make/make';
+import Home from './home/home';
+import Preview from './preview/preview';
+import MyPage from './mypage/mypage';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Make />} />
+          <Route path="/home" element={<Home />} />
+          <Route path='/preview' element={<Preview />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
---
+++ src/mypage/mypage.tsx
@@ -0,0 +1,10 @@
+const MyPage = () => {
+    return (
+      <div>
+        <h1>MyPage</h1>
+      </div>
+    );
+  };
+  
+  export default MyPage;
+  
