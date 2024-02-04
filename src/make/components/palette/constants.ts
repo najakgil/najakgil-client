66a9bf350dd08b962fd3228f9564b28f6c800732
anonymousrecords <a.Records2022@gmail.com>
2024-02-04 20:13:21 +0900

    feat : palette 컴포넌트 구현

---
+++ src/make/components/palette/constants.ts
@@ -0,0 +1,183 @@
+export const tabList = [
+  {
+    id: 1,
+    title: '캐릭터',
+  },
+  {
+    id: 2,
+    title: '꾸미기',
+  },
+  {
+    id: 3,
+    title: '배경',
+  },
+];
+
+export const tagList = {
+  character: [
+    {
+      id: 1,
+      tag: 'winter',
+      title: '겨울 외투',
+    },
+    {
+      id: 2,
+      tag: 'summer',
+      title: '바캉스룩',
+    },
+    {
+      id: 3,
+      tag: 'scrub',
+      title: '의사가운',
+    },
+    {
+      id: 4,
+      tag: 'suit',
+      title: '정장',
+    },
+    {
+      id: 5,
+      tag: 'graduationsuit',
+      title: '학위복',
+    },
+    {
+      id: 6,
+      tag: 'fhanbok',
+      title: '여자 한복',
+    },
+    {
+      id: 7,
+      tag: 'mhanbok',
+      title: '남자 한복',
+    },
+  ],
+} as const;
+
+export const characterImageSrc = {
+  winter: [
+    {
+      id: 1,
+      src: '/assets/image/character/winter-1.png',
+    },
+    {
+      id: 2,
+      src: '/assets/image/character/winter-2.png',
+    },
+    {
+      id: 3,
+      src: '/assets/image/character/winter-3.png',
+    },
+    {
+      id: 4,
+      src: '/assets/image/character/winter-4.png',
+    },
+  ],
+  summer: [
+    {
+      id: 1,
+      src: '/assets/image/character/summer-1.png',
+    },
+    {
+      id: 2,
+      src: '/assets/image/character/summer-2.png',
+    },
+    {
+      id: 3,
+      src: '/assets/image/character/summer-3.png',
+    },
+    {
+      id: 4,
+      src: '/assets/image/character/summer-4.png',
+    },
+  ],
+  scrub: [
+    {
+      id: 1,
+      src: '/assets/image/character/scrub-1.png',
+    },
+    {
+      id: 2,
+      src: '/assets/image/character/scrub-2.png',
+    },
+    {
+      id: 3,
+      src: '/assets/image/character/scrub-3.png',
+    },
+    {
+      id: 4,
+      src: '/assets/image/character/scrub-4.png',
+    },
+  ],
+  suit: [
+    {
+      id: 1,
+      src: '/assets/image/character/suit-1.png',
+    },
+    {
+      id: 2,
+      src: '/assets/image/character/suit-2.png',
+    },
+    {
+      id: 3,
+      src: '/assets/image/character/suit-3.png',
+    },
+    {
+      id: 4,
+      src: '/assets/image/character/suit-4.png',
+    },
+  ],
+  graduationsuit: [
+    {
+      id: 1,
+      src: '/assets/image/character/graduationsuit-1.png',
+    },
+    {
+      id: 2,
+      src: '/assets/image/character/graduationsuit-2.png',
+    },
+    {
+      id: 3,
+      src: '/assets/image/character/graduationsuit-3.png',
+    },
+    {
+      id: 4,
+      src: '/assets/image/character/graduationsuit-4.png',
+    },
+  ],
+  fhanbok: [
+    {
+      id: 1,
+      src: '/assets/image/character/fhanbok-1.png',
+    },
+    {
+      id: 2,
+      src: '/assets/image/character/fhanbok-2.png',
+    },
+    {
+      id: 3,
+      src: '/assets/image/character/fhanbok-3.png',
+    },
+    {
+      id: 4,
+      src: '/assets/image/character/fhanbok-4.png',
+    },
+  ],
+  mhanbok: [
+    {
+      id: 1,
+      src: '/assets/image/character/mhanbok-1.png',
+    },
+    {
+      id: 2,
+      src: '/assets/image/character/mhanbok-2.png',
+    },
+    {
+      id: 3,
+      src: '/assets/image/character/mhanbok-3.png',
+    },
+    {
+      id: 4,
+      src: '/assets/image/character/mhanbok-4.png',
+    },
+  ],
+};
