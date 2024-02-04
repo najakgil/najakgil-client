b8eb38b2a3a5966cccabcfd8fda05755e1d992ad
anonymousrecords <a.Records2022@gmail.com>
2024-02-04 20:14:30 +0900

    feat : 만들기 페이지 일부 구현

--- src/make/make.tsx
+++ src/make/make.tsx
@@ -1,8 +1,20 @@
+import Header from '../common/header/header';
+import Navigation from '../common/navigation/navigation';
+import Canvas from './components/canvas';
+import CompleteButton from './components/complete-button';
+import Palette from './components/palette/palette';
+import { useNavigate } from 'react-router-dom';
+
 const Make = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Make</h1>
-    </div>
+    <>
+      <Header />
+      <Canvas />
+      <Palette />
+      <CompleteButton onClick={() => navigate('/preview')}/>
+      <Navigation page="make" />
+    </>
   );
 };
 
