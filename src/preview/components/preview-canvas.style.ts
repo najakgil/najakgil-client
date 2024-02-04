50c55ead5fb735ceb7a5389fbc7a7e5b34aea276
anonymousrecords <a.Records2022@gmail.com>
2024-02-04 21:55:27 +0900

    feat : preview-canvas 컴포넌트 구현

---
+++ src/preview/components/preview-canvas.style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.img`
+  width: 100%;
+  aspect-ratio: 1/1;
+  background-color: pink;
+`;
---
+++ src/preview/components/preview-canvas.tsx
@@ -0,0 +1,12 @@
+import * as S from './preview-canvas.style.ts';
+import { useRecoilValue } from 'recoil';
+import { characterChoiceAtom } from '../../recoil/character-choice-atom';
+
+export default function PreviewCanvas() {
+  const canvasImageSrc = useRecoilValue(characterChoiceAtom);
+  return (
+    <>
+      <S.Wrapper src={canvasImageSrc} />
+    </>
+  );
+}
