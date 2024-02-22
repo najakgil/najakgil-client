6146c548ae18d3a350911e78f632385c15c3322e
anonymousrecords <a.Records2022@gmail.com>
2024-02-22 22:45:45 +0900

    feat : 홈 페이지 생성

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,8 +1,14 @@
+import HomeHeader from './components/home-header';
+import * as S from './style.css';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <HomeHeader />
+      <div className={S.base}>
+        <h1>Home</h1>
+      </div>
+    </>
   );
 };
 
---
+++ src/home/style.css.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/css';
+
+export const base = css({
+  marginTop: '39px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const buttonContainer = css({
+  display: 'flex',
+  width: '354px',
+  gap: '10px',
+});
