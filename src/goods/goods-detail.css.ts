b11f090cd0b873300f6f96ade2cd58eabb10e39c
anonymousrecords <a.Records2022@gmail.com>
2024-02-27 18:45:15 +0900

    feat : Goods Detail 페이지 구현

---
+++ src/goods/goods-detail.css.ts
@@ -0,0 +1,27 @@
+import { css } from '@emotion/css';
+
+export const wrapper = css`
+    padding: 20px;
+`;
+
+export const title = css`
+  font-size: 20px;
+  font-weight: 500;
+  margin-bottom: 10px;
+`;
+
+export const content = css`
+  font-size: 14px;
+  margin-top: 10px;
+`
+export const button = css`
+    position: absolute;
+    bottom: 20px;
+    width: 328px;
+    height: 44px;
+    background-color: #2294FF;
+    color: white;
+    font-weight: 500;
+    font-size: 18px;
+    border-radius: 8px;
+`
