3c828b03d1d4d47ec43fc9d9ff0f4c4476c377b9
anonymousrecords <a.Records2022@gmail.com>
2024-03-22 17:07:28 +0900

    fix : flex-direction 추가

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -11,6 +11,7 @@ export default function Layout({ children }: LayoutProps) {
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   min-width: 360px;
   max-width: 450px;
   height: 100vh;
