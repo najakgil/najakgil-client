308b6956cbfd2b95b9e931026d585627a383a377
anonymousrecords <a.Records2022@gmail.com>
2024-03-29 20:38:44 +0900

    feat : 펜 패널 구현

--- src/pages/make/_components/decoration-tab/brush-panel/brush-panel.tsx
+++ src/pages/make/_components/decoration-tab/brush-panel/brush-panel.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
+import { css } from '@emotion/react';
 import { useBrushPanelStore } from 'store/panel/useBrushPanelStore';
 
-const BrushPanel = () => {
-  const {brushColor, setBrushColor, brushSize, setBrushSize} = useBrushPanelStore();
-
+export default function BrushPanel() {
+  const { brushColor, setBrushColor, brushSize, setBrushSize } = useBrushPanelStore();
   const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setBrushColor(event.target.value);
   };
@@ -12,27 +13,45 @@ const BrushPanel = () => {
   };
 
   return (
-    <div>
-      <label htmlFor="brushColor">Brush Color:</label>
-      <input
-        type="color"
-        id="brushColor"
-        name="brushColor"
-        value={brushColor}
-        onChange={handleColorChange}
-      />
-      <label htmlFor="brushSize">Brush Size:</label>
-      <input
-        type="range"
-        id="brushSize"
-        name="brushSize"
-        min="1"
-        max="20"
-        value={brushSize}
-        onChange={handleSizeChange}
-      />
+    <div css={wrapper}>
+      <div css={contentBox}>
+        <label css={title}>펜 색상</label>
+        <input
+          type="color"
+          id="brushColor"
+          name="brushColor"
+          value={brushColor}
+          onChange={handleColorChange}
+        />
+      </div>
+      <div css={contentBox}>
+        <label css={title}>펜 굵기</label>
+        <input
+          type="range"
+          id="brushSize"
+          name="brushSize"
+          min="1"
+          max="20"
+          value={brushSize}
+          onChange={handleSizeChange}
+        />
+      </div>
     </div>
   );
-};
+}
 
-export default BrushPanel;
+export const wrapper = css({
+  padding: '20px',
+});
+
+export const contentBox = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '7px',
+  marginBottom: '20px',
+});
+
+export const title = css({
+  fontSize: '14px',
+  fontWeight: 'bold',
+});
