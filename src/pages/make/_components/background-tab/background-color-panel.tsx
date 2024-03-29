971974b9acae78b243850994b825e5d3fc0886a5
anonymousrecords <a.Records2022@gmail.com>
2024-03-29 20:38:16 +0900

    feat : 배경색 패널 구현

--- src/pages/make/_components/background-tab/background-color-panel.tsx
+++ src/pages/make/_components/background-tab/background-color-panel.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import { useBackgroundTabStore } from 'store/tab/useBackgroundTabStore';
 
 export default function BackgroundColorPanel() {
@@ -7,15 +8,33 @@ export default function BackgroundColorPanel() {
     setActiveBackgroundColor(event.target.value);
   };
   return (
-    <>
-      <label htmlFor="brushColor">Background Color:</label>
-      <input
-        type="color"
-        id="brushColor"
-        name="brushColor"
-        value={activeBackgroundColor}
-        onChange={handleColorChange}
-      />
-    </>
+    <div css={wrapper}>
+      <div css={contentBox}>
+        <label css={title}>배경 색상</label>
+        <input
+          type="color"
+          id="brushColor"
+          name="brushColor"
+          value={activeBackgroundColor}
+          onChange={handleColorChange}
+        />
+      </div>
+    </div>
   );
 }
+
+export const wrapper = css({
+  padding: '20px',
+});
+
+export const contentBox = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '7px',
+  marginBottom: '20px',
+});
+
+export const title = css({
+  fontSize: '14px',
+  fontWeight: 'bold',
+});
