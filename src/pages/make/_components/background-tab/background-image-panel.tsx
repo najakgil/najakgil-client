0b5d213547158fd4eee36e2e970360c1a304aebe
anonymousrecords <a.Records2022@gmail.com>
2024-03-29 20:38:25 +0900

    feat : 배경 이미지 패널 구현

--- src/pages/make/_components/background-tab/background-image-panel.tsx
+++ src/pages/make/_components/background-tab/background-image-panel.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import { useBackgroundTabStore } from 'store/tab/useBackgroundTabStore';
 
 export default function BackgroundImagePanel() {
@@ -17,15 +18,40 @@ export default function BackgroundImagePanel() {
   };
 
   return (
-    <div>
-      <label htmlFor="backgroundImage">Background Image:</label>
-      <input
-        type="file"
-        id="backgroundImage"
-        name="backgroundImage"
-        accept="image/*"
-        onChange={handleFileSelect}
-      />
+    <div css={wrapper}>
+      <div css={contentBox}>
+        <label css={title}>배경 이미지</label>
+        <input
+          type="file"
+          id="backgroundImage"
+          name="backgroundImage"
+          accept="image/*"
+          onChange={handleFileSelect}
+          style = {{
+            padding: '10px',
+            border: '1px solid #e0e0e0',
+            borderRadius: '5px',
+            fontSize: '14px',
+            backgroundColor: '#f9f9f9',
+          }}
+        />
+      </div>
     </div>
   );
 }
+
+export const wrapper = css({
+  padding: '20px',
+});
+
+export const contentBox = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '7px',
+  marginBottom: '20px',
+});
+
+export const title = css({
+  fontSize: '14px',
+  fontWeight: 'bold',
+});
